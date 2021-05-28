74b7a8781090b7b08b0d408ced2eaf6c3bca69b3
Matthew Piedra <matthewpiedra1228@gmail.com>
2021-05-28 11:46:16 -0400

    create project with cwa and add homepage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Homepage } from './pages';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="App__container">
+         <Homepage />
+      </main>
+    );
+  }
+}
